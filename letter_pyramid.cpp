14cf17f822a3394292bf55eed24e0e7134510fa6
Saurabh Kakade <saurabhKakade21@gmail.com>
2022-03-17 12:04:47 -0700

    Letter pyramid in C++

---
+++ letter_pyramid.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+
+using namespace std;
+
+int main()
+{
+    system("clear");
+    cout << "Enter the string for letter pyramid: ";
+    string input_string {};
+    // string space (input_string.length()-1,' ');
+    getline(cin,input_string);
+    size_t length = input_string.length();
+
+    for(size_t i=0; i < length; i++)
+    {
+        cout << " ";
+        for(size_t j = 0;j < length-i;j++)
+        {
+            cout << " ";
+        }
+
+        
+        string str = input_string.substr(0,i+1);
+        reverse(str.begin(), str.end());
+
+        cout << input_string.substr(0,i+1) + str.substr(1,str.length()-1);
+
+        for(size_t j = 0;j < i+1;j++)
+        {
+            cout << " ";
+        }
+                       
+        cout << endl;
+        
+    }
+
+
+    return 0;
+}
