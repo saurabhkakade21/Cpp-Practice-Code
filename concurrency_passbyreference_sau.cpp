29a629b07cde803dc24177b4caf14dd50e607f6e
Saurabh Kakade <saurabhKakade21@gmail.com>
2022-03-18 02:26:43 -0700

    Concurrency practice code

--- concurrency_passbyreference_sau.cpp
+++ concurrency_passbyreference_sau.cpp
@@ -3,15 +3,40 @@
 #include <iostream>
 #include <list>
 #include <thread>
-#include <string>
 
 std::list <int> big_data;
 const long SIZE = 50000000;
 
-
-void DownloadFunction(std::string file_argument)
+class String
 {
-    std::cout << "[DownloadFunction] Downloading started...." << file_argument << std::endl;
+    public:
+        String()
+        {
+            std::cout << "String()" << std::endl;
+        }
+        String(const String &)
+        {
+            std::cout << "String(const String&)" << std::endl;
+        }
+        String & operator = (const String&)
+        {
+            std::cout << "String & operator = (const String&)" << std::endl;
+            return *this;
+        }
+        ~String()
+        {
+            std::cout << "~String()" << std::endl;
+        }
+
+};
+
+// void DownloadFunction(String file_argument) for without pass by reference
+// void DownloadFunction(const String &file_argument) for pass by reference
+
+//pass by constant reference
+void DownloadFunction(const String &file_argument)
+{
+    std::cout << "[DownloadFunction] Downloading started...." << std::endl;
     for (size_t i = 0; i < SIZE; i++)
     {
         big_data.push_back(i);
@@ -23,10 +48,41 @@ void DownloadFunction(std::string file_argument)
 int main()
 {
     // system("clear");
-    std::string file_argument {"saurabh kakade"};
+    String file_argument;
     std::cout << "[Main] Step 01: Operation has started..." << std::endl;
 
-    std::thread thread_download(DownloadFunction, file_argument);
+    //following code will create two instances of String.
+    // std::thread thread_download(DownloadFunction, file_argument);
+    /* output for passing without reference
+        String()
+        [Main] Step 01: Operation has started...
+        String(const String&)
+        String(const String&)
+        ~String()
+        [Main] Step 02: Started another operation...
+        String(const String&)
+        [DownloadFunction] Fininshed Downloading....
+        ~String()
+        ~String()
+        ~String()
+    */
+
+
+    //so use passed by reference as threads are creating instances without it.
+    // std::thread thread_download(DownloadFunction, std::ref(file_argument));
+
+    /* Output from passing with reference
+        String()    //only one instance of SString is called.
+        [Main] Step 01: Operation has started...
+        [Main] Step 02: Started another operation...
+        [DownloadFunction] Downloading started....
+        [DownloadFunction] Fininshed Downloading....
+        ~String()
+    */
+
+    //pass by const reference use std::cref(argument)
+    std::thread thread_download(DownloadFunction, std::cref(file_argument)); 
+
     std::cout << "[Main] Step 02: Started another operation..." << std::endl;
 
     // thread_download.detach();
